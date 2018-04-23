1df0dfd40f0540bab842a00efa54ad0cc60bde52
Keith Tan <skeithtan@gmail.com>
2018-04-23 20:52:00 +0800

    Removed MuiThemeProvider

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import registerServiceWorker from "./registerServiceWorker";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
-import theme from "./theme";
 import "./index.css";
 import App from "./App";
 
 const app = (
-    <MuiThemeProvider muiTheme={theme}>
-        <App />
-    </MuiThemeProvider>
+    <App/>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
