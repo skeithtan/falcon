339c6d6b9778fef3b1faed7f72895a2d64399138
Keith Tan <skeithtan@gmail.com>
2018-04-27 19:24:51 +0800

    Replaced FalconAppBar with SignInPage in Ap.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import FalconAppBar from "./components/FalconAppBar";
+import SignInPage from "./pages/SignIn";
 import "./App.css";
 
 
@@ -8,7 +8,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <FalconAppBar/>
+                <SignInPage/>
             </div>
         );
     }
