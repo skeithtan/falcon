cb4f3d420531a4594112573e2b6428ece211c02c
Keith Tan <skeithtan@gmail.com>
2018-04-28 09:49:41 +0800

    Added graphiql, graphic endpoints to Express

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import bodyParser from "body-parser";
+import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+
 import config from "./config";
+import schema from "./graphql/schema";
+
 
 const app = express();
 
-app.get("/", (request, response) => {
-    response.send("Hello, World!");
-});
+app.use("/graphql", bodyParser.json(), graphqlExpress({schema}));
+app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
 
 app.listen(config.server.port, () => {
     console.info(`Server listening at port ${config.server.port}`);
