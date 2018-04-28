0adf3873db1e3312949c876fba7578be34a45024
Keith Tan <skeithtan@gmail.com>
2018-04-28 10:58:47 +0800

    Removed debugging logging leftover

--- server/graphql/resolvers/signin.js
+++ server/graphql/resolvers/signin.js
@@ -6,17 +6,13 @@ import { GraphQLError } from "graphql";
 export default {
     async signIn(object, args, context, info) {
         const {email, password} = args;
-
-        console.log("Finding user");
         const user = await User.findOne({email: email}).exec();
-        console.log(user);
 
         if (!user) {
             throw new AuthenticationError();
         }
 
         const isValidPassword = user.comparePassword(password);
-        console.log(isValidPassword);
 
         if (!isValidPassword) {
             throw new AuthenticationError();
