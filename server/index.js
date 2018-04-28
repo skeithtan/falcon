3513df2bc422d3437a42a69b98d75081a110deba
Keith Tan <skeithtan@gmail.com>
2018-04-28 10:57:20 +0800

    Added database connection to app start

--- server/index.js
+++ server/index.js
@@ -1,16 +1,26 @@
 import express from "express";
 import bodyParser from "body-parser";
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
+import mongoose from "mongoose";
+import "babel-polyfill"; //Needed for async/await operations
 
 import config from "./config";
 import schema from "./graphql/schema";
 
+mongoose.connect(config.database.url)
+    .then(onDatabaseConnect)
+    .catch(err => console.log(`Could not connect to mongodb: ${err}`));
 
-const app = express();
+function onDatabaseConnect() {
+    console.log("Connected to database");
 
-app.use("/graphql", bodyParser.json(), graphqlExpress({schema}));
-app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
+    const app = express();
 
-app.listen(config.server.port, () => {
-    console.info(`Server listening at port ${config.server.port}`);
-});
+    app.use("/graphql", bodyParser.json(), graphqlExpress({schema}));
+    app.use("/graphiql", graphiqlExpress({endpointURL: "/graphql"}));
+
+    app.listen(config.server.port, () => {
+        console.info(`Server listening at port ${config.server.port}`);
+    });
+}
+
