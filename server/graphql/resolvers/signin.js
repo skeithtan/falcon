e38ddce857871b4082762c45cfac511357b2d767
Keith Tan <skeithtan@gmail.com>
2018-04-28 10:57:54 +0800

    Added email/password validation to signIn resolver

--- server/graphql/resolvers/signin.js
+++ server/graphql/resolvers/signin.js
@@ -1,10 +1,31 @@
 import User from "../../models/user";
+import AuthenticationError from "../errors/authentication_error";
+import { GraphQLError } from "graphql";
 
 
 export default {
-    signIn(object, args, context, info) {
+    async signIn(object, args, context, info) {
         const {email, password} = args;
+
+        console.log("Finding user");
+        const user = await User.findOne({email: email}).exec();
+        console.log(user);
+
+        if (!user) {
+            throw new AuthenticationError();
+        }
+
+        const isValidPassword = user.comparePassword(password);
+        console.log(isValidPassword);
+
+        if (!isValidPassword) {
+            throw new AuthenticationError();
+        }
+
+        //TODO: MakeJWT
+
         //TODO: Return JWT
         return `Welcome, ${email}`;
+
     },
 };
