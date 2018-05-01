b9eafe01a5cc318694e637afedf229a4c84d8c43
Keith Tan <skeithtan@gmail.com>
2018-05-01 08:42:39 +0800

    Added redux dev tools support

--- client/src/store.js
+++ client/src/store.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 import reducers from "./reducers";
 
 
-const store = createStore(reducers);
+const store = createStore(reducers, composeWithDevTools());
 export default store;
