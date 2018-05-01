e48fc2fae0896918632263c2536e1fa712d95da8
Keith Tan <skeithtan@gmail.com>
2018-05-01 14:30:57 +0800

    Registered pages reducer to reducers index.js

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import authentication from "./authentication.reducer";
+import pages from "./pages.reducer";
 
 
 export default combineReducers({
     authentication,
+    pages,
 });
