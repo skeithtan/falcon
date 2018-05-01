c7c7cbd1fd2b6ab802909c7974d23625b8005fbc
Keith Tan <skeithtan@gmail.com>
2018-05-01 10:39:10 +0800

    Added FacultyProfile component

---
+++ client/src/pages/FacultyProfiles/FacultyProfiles.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+import "./FacultyProfiles.css";
+
+
+export default class FacultyProfilesPage extends Component {
+    render() {
+        //TODO: FacultyProfilesPage
+        return <h1>Faculty Profiles</h1>
+    }
+}
---
+++ client/src/pages/FacultyProfiles/index.js
@@ -0,0 +1,5 @@
+import { connect } from "react-redux";
+import FacultyProfilesPage from "./FacultyProfiles";
+
+
+export default connect(null, null)(FacultyProfilesPage);
