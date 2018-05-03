9c1fc8f076ecb12bee71bf1628ae1c9b0bc6af7f
Keith <skeithtan@gmail.com>
2018-05-03 17:57:05 +0800

    Added new errors/index.js

---
+++ server/graphql/errors/index.js
@@ -0,0 +1,9 @@
+import AuthenticationError from "./authentication.error";
+import AuthorizationError from "./authorization.error";
+import GuestAccessError from "./guest.error";
+
+export {
+    AuthenticationError,
+    AuthorizationError,
+    GuestAccessError,
+};
