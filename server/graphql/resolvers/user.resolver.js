1a1075fe2a487b238081ec41b8605b4bb1c219a5
Keith <skeithtan@gmail.com>
2018-05-03 17:55:14 +0800

    Updated user resolver to work with context.authorization

--- server/graphql/resolvers/user.resolver.js
+++ server/graphql/resolvers/user.resolver.js
@@ -1,39 +1,45 @@
-import User from "../../models/user.model";
-import AuthenticationError from "../errors/authentication.error";
 import jwt from "jsonwebtoken";
+
+import User from "../../models/user.model";
+import { requireSignIn } from "../../utils/user_decorator";
+import getUserFromToken from "../../utils/user_from_token";
+import { AuthenticationError } from "../errors";
 import config from "../../config";
 
+function currentUser(object, args, context) {
+    return getUserFromToken(context.authorization);
+}
+
+async function signIn(object, {email, password}) {
+    const user = await User.findOne({email: email}).exec();
+
+    if (!user) {
+        return new AuthenticationError();
+    }
+
+    const isValidPassword = user.comparePassword(password);
+
+    if (!isValidPassword) {
+        return new AuthenticationError();
+    }
+
+    const token = jwt.sign({
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+        photo: user.photo,
+        authorization: user.authorization,
+    }, config.server.jwtSecret);
+
+    return token;
+}
 
 const queryResolvers = {
-    async currentUser(object, args, context) {
-        return context.user;
-    },
+    currentUser: requireSignIn(currentUser),
 };
 
 const mutationResolvers = {
-    async signIn(object, {email, password}) {
-        const user = await User.findOne({email: email}).exec();
-
-        if (!user) {
-            throw new AuthenticationError();
-        }
-
-        const isValidPassword = user.comparePassword(password);
-
-        if (!isValidPassword) {
-            throw new AuthenticationError();
-        }
-
-        const token = jwt.sign({
-            _id: user._id,
-            email: user.email,
-            name: user.name,
-            photo: user.photo,
-            authorization: user.authorization,
-        }, config.server.jwtSecret);
-
-        return token;
-    },
+    signIn,
 };
 
 export { queryResolvers, mutationResolvers };
