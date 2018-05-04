9f717598e143d2233403864e9ea0624b5163452a
Keith <skeithtan@gmail.com>
2018-05-04 15:04:31 +0800

    Refactored queryExtension/mutationExtension to extensionTypes

--- server/graphql/schema.js
+++ server/graphql/schema.js
@@ -5,21 +5,20 @@ import resolvers from "./resolvers";
 
 
 const rootQuery = requireText("./schema.graphql", require);
-const types = ["date", "faculty", "class", "user"];
-const queryExtensionTypes = ["user", "class"];
-const mutationExtensionTypes = ["user", "class"];
+const extensionTypes = ["user", "class", "faculty"];
+const types = ["date", ...extensionTypes];
 
 const typeDefinitions = types
     .map(type => `${type}.type.graphql`) // Transform type to fileName
     .map(fileName => `./types/${fileName}`) // Transform fileName to path
     .map(path => requireText(path, require)); // Get file contents as string
 
-const queryExtensions = queryExtensionTypes
+const queryExtensions = extensionTypes
     .map(type => `${type}.query.graphql`)
     .map(fileName => `./query_extensions/${fileName}`)
     .map(path => requireText(path, require));
 
-const mutationExtensions = mutationExtensionTypes
+const mutationExtensions = extensionTypes
     .map(type => `${type}.mutation.graphql`)
     .map(fileName => `./mutation_extensions/${fileName}`)
     .map(path => requireText(path, require));
