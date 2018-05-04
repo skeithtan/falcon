c3c11064d5cdffe077000b78d29466e1a1a94f6c
Keith <skeithtan@gmail.com>
2018-05-04 11:47:58 +0800

    Added ValidationError graphq error

--- server/graphql/errors/index.js
+++ server/graphql/errors/index.js
@@ -1,9 +1,11 @@
 import AuthenticationError from "./authentication.error";
 import AuthorizationError from "./authorization.error";
 import GuestAccessError from "./guest.error";
+import ValidationError from "./validation.error";
 
 export {
     AuthenticationError,
     AuthorizationError,
     GuestAccessError,
+    ValidationError,
 };
---
+++ server/graphql/errors/validation.error.js
@@ -0,0 +1,7 @@
+import { GraphQLError } from "graphql/error";
+
+export default class ValidationError extends GraphQLError {
+    constructor(error) {
+        super(`ValidationError: ${error}`);
+    }
+}
