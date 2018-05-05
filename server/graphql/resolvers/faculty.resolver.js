b6c9073e9c6b4a19406b4ec5462aa12a490b45af
Keith <skeithtan@gmail.com>
2018-05-05 11:12:56 +0800

    Added update operations to faculty fields

--- server/graphql/resolvers/faculty.resolver.js
+++ server/graphql/resolvers/faculty.resolver.js
@@ -2,7 +2,6 @@ import { Faculty } from "../../models/faculty.model";
 import { User } from "../../models/user.model";
 import { limitAccess, NO_FACULTY } from "../../utils/user_decorator";
 import { FACULTY } from "../../models/user.model";
-import ValidationError from "../errors/validation.error";
 
 function faculties() {
     return Faculty.find({}).populate("user");
@@ -31,6 +30,10 @@ async function createFaculty(object, args) {
     }
 }
 
+function updateFaculty(object, {_id, newFaculty}) {
+    return Faculty.findByIdAndUpdate(_id, newFaculty, {new: true});
+}
+
 async function createPresentation(object, args) {
     const presentationInput = args.presentation;
     const facultyId = args.facultyId;
@@ -40,6 +43,17 @@ async function createPresentation(object, args) {
     await faculty.save(); //This must be await so errors are thrown before returning
 
     return faculty.presentations[faculty.presentations.length - 1];
+}
+
+async function updatePresentation(object, args) {
+    const {_id, newPresentation, facultyId} = args;
+    const faculty = await Faculty.findById({_id: facultyId}).exec();
+    const presentation = faculty.presentations.id(_id);
+
+    Object.assign(presentation, newPresentation);
+    await faculty.save();
+
+    return presentation;
 }
 
 async function createRecognition(object, args) {
@@ -53,6 +67,17 @@ async function createRecognition(object, args) {
     return faculty.recognitions[faculty.recognitions.length - 1];
 }
 
+async function updateRecognition(object, args) {
+    const {_id, newRecognition, facultyId} = args;
+    const faculty = await Faculty.findById({_id: facultyId}).exec();
+    const recognition = faculty.recognitions.id(_id);
+
+    Object.assign(recognition, newRecognition);
+    await faculty.save();
+
+    return recognition;
+}
+
 async function createInstructionalMaterial(object, args) {
     const instructionalMaterialInput = args.instructionalMaterial;
     const facultyId = args.facultyId;
@@ -62,6 +87,17 @@ async function createInstructionalMaterial(object, args) {
     await faculty.save();
 
     return faculty.instructionalMaterials[faculty.instructionalMaterials.length - 1];
+}
+
+async function updateInstructionalMaterial(object, args) {
+    const {_id, newInstructionalMaterial, facultyId} = args;
+    const faculty = await Faculty.findById({_id: facultyId}).exec();
+    const instructionalMaterial = faculty.instructionalMaterials.id(_id);
+
+    Object.assign(instructionalMaterial, newInstructionalMaterial);
+    await faculty.save();
+
+    return instructionalMaterial;
 }
 
 async function createExtensionWork(object, args) {
@@ -75,6 +111,17 @@ async function createExtensionWork(object, args) {
     return faculty.extensionWorks[faculty.extensionWorks.length - 1];
 }
 
+async function updateExtensionWork(object, args) {
+    const {_id, newExtensionWork, facultyId} = args;
+    const faculty = await Faculty.findById({_id: facultyId}).exec();
+    const extensionWork = faculty.extensionWorks.id(_id);
+
+    Object.assign(extensionWork, newExtensionWork);
+    await faculty.save();
+
+    return extensionWork;
+}
+
 
 export const queryResolvers = {
     faculties: limitAccess(faculties, {allowed: NO_FACULTY, action: "Get all faculties"}),
@@ -82,10 +129,21 @@ export const queryResolvers = {
 
 export const mutationResolvers = {
     createFaculty: limitAccess(createFaculty, {allowed: NO_FACULTY, action: "Create faculty"}),
+    updateFaculty: limitAccess(updateFaculty, {allowed: NO_FACULTY, action: "Update faculty"}),
+
     createPresentation: limitAccess(createPresentation, {allowed: NO_FACULTY, action: "Create presentation"}),
+    updatePresentation: limitAccess(updatePresentation, {allowed: NO_FACULTY, action: "Update presentation"}),
+
     createRecognition: limitAccess(createRecognition, {allowed: NO_FACULTY, action: "Create recognition"}),
+    updateRecognition: limitAccess(updateRecognition, {allowed: NO_FACULTY, action: "Update recognition"}),
+
     createInstructionalMaterial: limitAccess(createInstructionalMaterial,
         {allowed: NO_FACULTY, action: "Create instructional materials"}),
+    updateInstructionalMaterial: limitAccess(updateInstructionalMaterial,
+        {allowed: NO_FACULTY, action: "Update instructional materials"}),
+
     createExtensionWork: limitAccess(createExtensionWork,
         {allowed: NO_FACULTY, action: "Create extension work"}),
+    updateExtensionWork: limitAccess(updateExtensionWork,
+        {allowed: NO_FACULTY, action: "Update extension work"}),
 };
