51db346fa3c5bf7dcc53a064ab2a0364252f75b5
Keith <skeithtan@gmail.com>
2018-05-06 12:30:58 +0800

    Set FacultyList to display actual data

--- client/src/pages/FacultyProfiles/components/FacultyList/FacultyList.js
+++ client/src/pages/FacultyProfiles/components/FacultyList/FacultyList.js
@@ -1,98 +1,73 @@
 import React, { Component } from "react";
-import ListSubheader from "material-ui/List/ListSubheader";
-import List, { ListItem, ListItemText } from "material-ui/List";
+import List, { ListItem } from "material-ui/List";
+import Typography from "material-ui/Typography";
 import Avatar from "material-ui/Avatar";
 import Button from "material-ui/Button";
-import AddIcon from '@material-ui/icons/Add';
+import AddIcon from "@material-ui/icons/Add";
+import Grid from "material-ui/Grid";
+import { CircularProgress } from "material-ui/Progress";
+
+
+class FacultyItem extends Component {
+    render() {
+        const classes = this.props.classes;
+
+        return (
+            <ListItem className={this.props.active && classes.activeListItem}
+                      button
+                      dense>
+                <Grid container wrap="nowrap" spacing={16} alignItems="center">
+                    <Grid item>
+                        <Avatar>PN</Avatar>
+                    </Grid>
+                    <Grid item>
+                        <Typography className={this.props.active && classes.activeListItemText}
+                                    variant="subheading">{this.props.children}</Typography>
+                    </Grid>
+                </Grid>
+            </ListItem>
+        );
+    }
+}
 
 export default class FacultyList extends Component {
+
+    renderList = faculties => (
+        <List>
+            {faculties.map(faculty =>
+                <FacultyItem classes={this.props.classes}
+                             key={faculty._id}>
+                    {faculty.user.name.first} {faculty.user.name.last}
+                </FacultyItem>
+            )}
+        </List>
+    );
+
+    loadingIndicator = () => (
+        <div className={this.props.classes.loadingIndicatorWrapper}>
+            <CircularProgress size={100} />
+        </div>
+    );
+
+    componentDidMount() {
+        this.props.fetchData();
+    }
+
     render() {
-        const {classes} = this.props;
+        const {classes, isLoading, faculties, errors} = this.props;
+
+        if (errors) {
+            console.log(this.props);
+        }
 
         return (
             <div className={classes.facultyList}>
-                <List subheader={<li />}>
-                    <li className={classes.listSection}>
-                        <ul className={classes.ul}>
-                            <ListSubheader>Full Time Permanent</ListSubheader>
-                            <ListItem button>
-                                <Avatar>RM</Avatar>
-                                <ListItemText>Reeve Musk</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>PN</Avatar>
-                                <ListItemText>Patrick Ng</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>PM</Avatar>
-                                <ListItemText>Peter Mendoza</ListItemText>
-                            </ListItem>
-                        </ul>
-                    </li>
-                    <li className={classes.listSection}>
-                        <ul className={classes.ul}>
-                            <ListSubheader>Full Time Temporary</ListSubheader>
-                            <ListItem button>
-                                <Avatar>CA</Avatar>
-                                <ListItemText>Claire Arguelles</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>SC</Avatar>
-                                <ListItemText>Samuel Conanan</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>RD</Avatar>
-                                <ListItemText>Ronald Deniega</ListItemText>
-                            </ListItem>
-                        </ul>
-                    </li>
-                    <li className={classes.listSection}>
-                        <ul className={classes.ul}>
-                            <ListSubheader>Part Time</ListSubheader>
-                            <ListItem button>
-                                <Avatar>HO</Avatar>
-                                <ListItemText>Hussein Obama</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>AP</Avatar>
-                                <ListItemText>Aaron Presley</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>JM</Avatar>
-                                <ListItemText>James McCartney</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>HG</Avatar>
-                                <ListItemText>Henry Gates</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>PJ</Avatar>
-                                <ListItemText>Paul Jobs</ListItemText>
-                            </ListItem><ListItem button>
-                            <Avatar>HO</Avatar>
-                            <ListItemText>Hussein Obama</ListItemText>
-                        </ListItem>
-                            <ListItem button>
-                                <Avatar>AP</Avatar>
-                                <ListItemText>Aaron Presley</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>JM</Avatar>
-                                <ListItemText>James McCartney</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>HG</Avatar>
-                                <ListItemText>Henry Gates</ListItemText>
-                            </ListItem>
-                            <ListItem button>
-                                <Avatar>PJ</Avatar>
-                                <ListItemText>Paul Jobs</ListItemText>
-                            </ListItem>
-                        </ul>
-                    </li>
-                </List>
+
+                {faculties && this.renderList(faculties)}
+                {isLoading && this.loadingIndicator()}
+
                 <Button variant="fab" color="primary" className={classes.addButton}>
-                    <AddIcon/>
+                    <AddIcon />
                 </Button>
             </div>
         );
