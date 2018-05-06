a8d4a32f00dae4c0d2eb2066e5aa23fbf0c882f3
Keith <skeithtan@gmail.com>
2018-05-06 12:30:43 +0800

    Added data fetching, connected redux to FacultyList

--- client/src/pages/FacultyProfiles/components/FacultyList/index.js
+++ client/src/pages/FacultyProfiles/components/FacultyList/index.js
@@ -1,10 +1,42 @@
 import { compose } from "recompose";
+import { connect } from "react-redux";
 import { withTheme, withStyles } from "material-ui/styles";
+import { setFaculties, startLoading, setErrors } from "../../../../actions/faculty_list.actions";
 
 import style from "./styles";
 import FacultyList from "./FacultyList";
+import { getAllFacultiesOverview } from "../../../../services/faculty.service";
+
+function mapStateToProps(state) {
+    return {
+        isLoading: state.facultyList.isLoading,
+        faculties: state.facultyList.faculties,
+        errors: state.facultyList.errors,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchData() {
+            dispatch(startLoading());
+
+            getAllFacultiesOverview()
+                .then(query => {
+                    if (query.data) {
+                        dispatch(setFaculties(query.data.faculties));
+                    }
+
+                    if (query.errors) {
+                        dispatch(setErrors(query.errors));
+                    }
+                });
+        },
+    };
+}
+
 
 export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
     withTheme(),
     withStyles(style),
 )(FacultyList);
