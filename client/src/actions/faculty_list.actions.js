25a060f2755f53aa07822601b5f4b86d77863ce3
Keith <skeithtan@gmail.com>
2018-05-07 11:55:46 +0800

    Added set active faculty, set search keyword to faculty list state

--- client/src/actions/faculty_list.actions.js
+++ client/src/actions/faculty_list.actions.js
@@ -1,6 +1,8 @@
 export const START_LOADING = "START_LOADING";
 export const SET_FACULTIES = "SET_FACULTIES";
 export const SET_ERRORS = "SET_ERRORS";
+export const SET_SEARCH_KEYWORD = "SET_SEARCH_KEYWORD";
+export const SET_ACTIVE_FACULTY = "SET_ACTIVE_FACULTY";
 
 export function startLoading(isLoading) {
     return {
@@ -19,5 +21,19 @@ export function setErrors(errors) {
     return {
         type: SET_ERRORS,
         errors,
+    };
+}
+
+export function setActiveFaculty(faculty) {
+    return {
+        type: SET_ACTIVE_FACULTY,
+        faculty,
+    };
+}
+
+export function setSearchKeyword(searchKeyword) {
+    return {
+        type: SET_SEARCH_KEYWORD,
+        searchKeyword,
     };
 }
--- client/src/reducers/faculty_list.reducer.js
+++ client/src/reducers/faculty_list.reducer.js
@@ -1,30 +1,51 @@
-import { SET_ERRORS, SET_FACULTIES, START_LOADING } from "../actions/faculty_list.actions";
+import {
+    SET_ACTIVE_FACULTY,
+    SET_ERRORS,
+    SET_FACULTIES,
+    SET_SEARCH_KEYWORD,
+    START_LOADING,
+} from "../actions/faculty_list.actions";
 
 const initialState = {
     faculties: null,
     isLoading: false,
     errors: null,
+    searchKeyword: "",
+    activeFaculty: null,
 };
 
 export default function facultyList(state = initialState, action) {
     switch (action.type) {
         case START_LOADING:
             return {
+                ...state,
                 isLoading: true,
                 faculties: null,
                 errors: null,
             };
         case SET_ERRORS:
             return {
+                ...state,
                 isLoading: false,
                 faculties: null,
                 errors: action.errors,
             };
         case SET_FACULTIES:
             return {
+                ...state,
                 isLoading: false,
                 faculties: action.faculties,
                 errors: null,
+            };
+        case SET_ACTIVE_FACULTY:
+            return {
+                ...state,
+                activeFaculty: action.faculty,
+            };
+        case SET_SEARCH_KEYWORD:
+            return {
+                ...state,
+                searchKeyword: action.searchKeyword,
             };
         default:
             return state;
