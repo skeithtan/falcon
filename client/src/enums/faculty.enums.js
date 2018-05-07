6f41f6355778d5f17b517e88e7d3d5e954acfb70
Keith <skeithtan@gmail.com>
2018-05-07 14:15:03 +0800

    Defined faculty enums, human wording translation

---
+++ client/src/enums/faculty.enums.js
@@ -0,0 +1,68 @@
+export default {
+    EMPLOYMENT: {
+        "FULL_TIME_PERMANENT": "Permanent Full Time",
+        "FULL_TIME_TEMPORARY": "Temporary Full Time",
+        "PART_TIME": "Part Time",
+    },
+    DEGREE: {
+        "ASSOCIATE": "Associate",
+        "BACHELOR": "Bachelor",
+        "MASTER": "Master",
+        "DOCTORATE": "Doctorate",
+    },
+
+    EXTENSION_WORK: {
+        ROLES: {
+            "LECTURER": "Lecturer",
+            "TRAINER": "Trainer",
+            "RESOURCE_SPEAKER": "Resource Speaker",
+            "FACILITATOR": "Facilitator",
+            "COACH": "Coach",
+            "MATERIAL_WRITER": "Material Writer",
+        },
+    },
+
+    INSTRUCTIONAL_MATERIAL: {
+        NON_PRINT_TYPES: {
+            "MODULE": "Module",
+            "VIDEO": "Video",
+            "SLIDE": "Slide",
+            "DIGITAL_FILE": "Digital File",
+            "AUDIO": "Audio",
+        },
+
+        MEDIUM: {
+            "PRINT": "Print",
+            "NON_PRINT": "Non-Print",
+        },
+
+        CLASSIFICATION: {
+            "STUDENT": "Student",
+            "TEACHER": "Teacher",
+        },
+    },
+
+    RECOGNITION: {
+        BASIS: {
+            "RESEARCH": "Research",
+            "SCHOLARSHIP": "Scholarship",
+            "EXTENSION_WORK": "Extension Work",
+            "CIVIC": "Civic",
+        },
+    },
+
+    PRESENTATION: {
+        CATEGORY: {
+            "INSTITUTIONAL": "Institutional",
+            "REGIONAL": "Regional",
+            "NATIONAL": "National",
+            "INTERNATIONAL": "International",
+        },
+        MEDIUM: {
+            "PAPER": "Paper",
+            "POSTER": "Poster",
+            "RESEARCH": "Research",
+        },
+    },
+
+};
