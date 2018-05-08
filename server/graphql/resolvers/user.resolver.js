0705a31db8e4b6e8c4faacf8fdb1fa61ceb66700
Keith <skeithtan@gmail.com>
2018-05-08 09:07:16 +0800

    Fixed currentUser query bug, added updateUser, changeCurrentUserPassword resolvers

--- server/graphql/resolvers/user.resolver.js
+++ server/graphql/resolvers/user.resolver.js
@@ -2,12 +2,12 @@ import jwt from "jsonwebtoken";
 
 import { User } from "../../models/user.model";
 import { requireSignIn } from "../../utils/user_decorator";
-import { AuthenticationError } from "../errors";
+import { AuthenticationError, DoesNotExistError } from "../errors";
 import { getUserFromContext } from "../../utils/user_from_context";
 import config from "../../config";
 
 function currentUser(object, args, context) {
-    return getUserFromContext(context.authorization);
+    return getUserFromContext(context);
 }
 
 async function signIn(object, {email, password}) {
@@ -34,10 +34,30 @@ async function signIn(object, {email, password}) {
     return token;
 }
 
+function updateUser(object, {_id, newUser}) {
+    return User.findByIdAndUpdate(_id, newUser, {new: true})
+               .exec()
+               .catch(() => {
+                   return new DoesNotExistError(`User of id ${_id} does not exist.`);
+               });
+}
+
+function changeCurrentUserPassword(object, {newPassword}, context) {
+    return getUserFromContext(context)
+        .then(user => {
+            user.secret = newPassword;
+            // Return true because return type is Boolean, true meaning success
+            // Errors will not return false, instead an error array from this uncaught promise
+            return user.save().then(() => true);
+        });
+}
+
 export const queryResolvers = {
     currentUser: requireSignIn(currentUser),
 };
 
 export const mutationResolvers = {
     signIn,
+    updateUser,
+    changeCurrentUserPassword,
 };
