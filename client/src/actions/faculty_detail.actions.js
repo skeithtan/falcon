1b2061546e8282e46d0a3e13cf151a0a42cb7191
Keith <skeithtan@gmail.com>
2018-05-08 14:06:31 +0800

    Created overview tab actions

--- client/src/actions/faculty_detail.actions.js
+++ client/src/actions/faculty_detail.actions.js
@@ -1,8 +1,30 @@
 export const SET_ACTIVE_TAB = "SET_CURRENT_TAB";
+export const OVERVIEW_IS_LOADING = "OVERVIEW_IS_LOADING";
+export const OVERVIEW_FETCHED = "OVERVIEW_FETCHED";
+export const OVERVIEW_FETCH_ERROR = "OVERVIEW_FETCH_ERROR";
 
 export function setActiveTab(tab) {
     return {
         type: SET_ACTIVE_TAB,
-        tab
-    }
+        tab,
+    };
+}
+
+export function overviewIsLoading() {
+    return {
+        type: OVERVIEW_IS_LOADING,
+    };
+}
+
+export function overviewFetched() {
+    return {
+        type: OVERVIEW_FETCHED,
+    };
+}
+
+export function overviewFetchError(errors) {
+    return {
+        type: OVERVIEW_FETCH_ERROR,
+        errors,
+    };
 }
