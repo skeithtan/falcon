88841d4b63067f162b4332eff929d3714e7f132e
Keith <skeithtan@gmail.com>
2018-05-10 19:19:52 +0800

    Set redux to reset state on user sign out

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,20 @@
 import { combineReducers } from "redux";
+import { SIGN_OUT_SUCCESS } from "../actions/authentication.actions";
 import authentication from "./authentication.reducer";
 import facultyProfiles from "./faculty_profiles.reducer";
 import pages from "./pages.reducer";
 
 
-export default combineReducers({
+const appReducer = combineReducers({
     authentication,
     pages,
     facultyProfiles,
 });
+
+export default (state, action) => {
+    if (action.type === SIGN_OUT_SUCCESS) {
+        state = undefined;
+    }
+
+    return appReducer(state, action);
+}
