e80d2c47865da9f2255fee3cd1f74f2c2b81f034
Keith <skeithtan@gmail.com>
2018-05-11 20:42:47 +0800

    Replaced FacultyAvatar with UserAvatar

---
+++ client/src/components/UserAvatar/UserAvatar.js
@@ -0,0 +1,27 @@
+import Avatar from "material-ui/Avatar";
+import React, { Component } from "react";
+import { getInitials, getFullName } from "../../utils/user";
+
+
+export default class UserAvatar extends Component {
+    renderInitials = (initials, fullName) => (
+        <Avatar className={this.props.className} onClick={this.props.onClick} alt={fullName}>{initials}</Avatar>
+    );
+
+    renderPhoto = (photo, fullName) => (
+        <Avatar className={this.props.className} onClick={this.props.onClick} alt={fullName} src={photo} />
+    );
+
+    render() {
+        const user = this.props.user;
+        const fullName = getFullName(user);
+        const initials = getInitials(user);
+        const photo = user.photo;
+
+        if (photo) {
+            return this.renderPhoto(photo, fullName);
+        }
+
+        return this.renderInitials(initials, fullName);
+    }
+}
