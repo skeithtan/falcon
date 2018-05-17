6865bb90445239ee680ee358b7da8429abc9f131
Keith <skeithtan@gmail.com>
2018-05-17 11:31:27 +0800

    Set TableRowActions to not wrap the icons

--- client/src/components/TableRowActions/TableRowActions.js
+++ client/src/components/TableRowActions/TableRowActions.js
@@ -1,3 +1,4 @@
+import Grid from "@material-ui/core/es/Grid";
 import IconButton from "@material-ui/core/IconButton";
 import TableCell from "@material-ui/core/TableCell";
 import Tooltip from "@material-ui/core/Tooltip";
@@ -17,21 +18,27 @@ class TableRowActions extends Component {
 
         return (
             <TableCell>
-                {onUpdateButtonClick &&
-                <Tooltip title={updateButtonTooltipTitle}>
-                    <IconButton aria-label="update" onClick={onUpdateButtonClick}>
-                        <EditIcon />
-                    </IconButton>
-                </Tooltip>
-                }
+                <Grid container wrap="nowrap">
+                    {onUpdateButtonClick &&
+                    <Grid item>
+                        <Tooltip title={updateButtonTooltipTitle}>
+                            <IconButton aria-label="update" onClick={onUpdateButtonClick}>
+                                <EditIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                    }
 
-                {onRemoveButtonClick &&
-                <Tooltip title={removeButtonTooltipTitle}>
-                    <IconButton aria-label="remove" onClick={onRemoveButtonClick}>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-                }
+                    {onRemoveButtonClick &&
+                    <Grid item>
+                        <Tooltip title={removeButtonTooltipTitle}>
+                            <IconButton aria-label="remove" onClick={onRemoveButtonClick}>
+                                <DeleteIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </Grid>
+                    }
+                </Grid>
             </TableCell>
         );
     }
