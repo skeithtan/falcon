56b9e84992a062e0806b0e3d7557e4e3e36a861f
Keith <skeithtan@gmail.com>
2018-05-19 18:10:09 +0800

    Added padding to faculty loading header

--- client/src/pages/FacultyLoading/components/FacultyLoadingHeader/styles.js
+++ client/src/pages/FacultyLoading/components/FacultyLoadingHeader/styles.js
@@ -2,6 +2,8 @@ export default theme => ({
     header: {
         background: theme.palette.primary.main,
         minHeight: 52,
+        paddingLeft: theme.spacing.unit * 3,
+        paddingRight: theme.spacing.unit * 3,
     },
     tabs: {
         color: "white",
