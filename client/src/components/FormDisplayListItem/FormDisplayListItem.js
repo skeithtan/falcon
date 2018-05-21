a95d528ef8a6cb8ffc01e48575806caecc2b7acb
Keith <skeithtan@gmail.com>
2018-05-21 08:44:34 +0800

    Changed FormDisplayListItem field name type
    Changed from ListItemText to Typography

--- client/src/components/FormDisplayListItem/FormDisplayListItem.js
+++ client/src/components/FormDisplayListItem/FormDisplayListItem.js
@@ -1,5 +1,6 @@
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import Typography from "@material-ui/core/Typography";
 import React, { Component } from "react";
 
 
@@ -9,7 +10,7 @@ export default class FormDisplayListItem extends Component {
         return (
             <ListItem divider>
                 <div className={classes.gridContainer}>
-                    <ListItemText><b>{field}</b></ListItemText>
+                    <Typography variant="body2">{field}</Typography>
                     <ListItemText>{value}</ListItemText>
                 </div>
             </ListItem>
