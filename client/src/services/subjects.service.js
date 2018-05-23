fa8e41239df7a6f1f47581f3357fe5d7d0bac553
Keith <skeithtan@gmail.com>
2018-05-24 07:59:22 +0800

    Added addSubject and updateSubejct to subjects service

--- client/src/services/subjects.service.js
+++ client/src/services/subjects.service.js
@@ -2,19 +2,58 @@ import gql from "graphql-tag";
 import client from "../client";
 
 
+const subjectFields = `
+    _id
+    code
+    name
+    faculties {
+        _id
+    }
+`;
+
 export function fetchAllSubjects() {
     return client.query({
         query: gql`
             query {
                 subjects {
-                    _id
-                    code
-                    name
-                    faculties {
-                        _id
+                    ${subjectFields}
+                }
+            }
+        `,
+    });
+}
+
+export function addSubject(newSubject) {
+    return client.mutate({
+        mutation: gql`
+            mutation addSubject($newSubject: SubjectInput!) {
+                subject {
+                    create(newSubject: $newSubject) {
+                        ${subjectFields}
                     }
                 }
             }
-        `
-    })
+        `,
+        variables: {
+            newSubject,
+        },
+    });
+}
+
+export function updateSubject(_id, newSubject) {
+    return client.mutate({
+        mutation: gql`
+            mutation updateSubject($_id: ID!, $newSubject: SubjectInput!) {
+                subject {
+                    update(_id: $_id, newSubject: $newSubject) {
+                        ${subjectFields}
+                    }
+                }
+            }
+        `,
+        variables: {
+            _id,
+            newSubject,
+        },
+    });
 }
