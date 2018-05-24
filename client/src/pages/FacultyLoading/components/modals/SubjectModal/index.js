5bf2f095b16967261373792be7dfb8be4b802a30
Keith <skeithtan@gmail.com>
2018-05-24 08:10:39 +0800

    Added SubjectModal to FacultyLoading

---
+++ client/src/pages/FacultyLoading/components/modals/SubjectModal/index.js
@@ -0,0 +1,45 @@
+import { withStyles } from "@material-ui/core/styles";
+import { connect } from "react-redux";
+import compose from "recompose/compose";
+import { getFetchFacultyListThunk } from "../../../../../utils/faculty.util";
+import { subjectIsAdded, subjectIsUpdated } from "../../../../../actions/subject.actions";
+import { addSubject, updateSubject } from "../../../../../services/subjects.service";
+import styles from "./styles";
+import SubjectModal from "./SubjectModal";
+
+
+function mapStateToProps(state) {
+    return {
+        ...state.faculty,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchData() {
+            return dispatch(getFetchFacultyListThunk());
+        },
+        submitAddSubject(form) {
+            return addSubject(form)
+                .then(result => {
+                    const subject = result.data.subject.create;
+                    dispatch(subjectIsAdded(subject));
+                    return subject;
+                })
+        },
+        submitUpdateSubject(subject, form) {
+            return updateSubject(subject._id, form)
+                .then(result => {
+                    const subject = result.data.subject.update;
+                    console.log("Returned subject", subject);
+                    dispatch(subjectIsUpdated(subject));
+                    return subject;
+                })
+        }
+    };
+}
+
+export default compose(
+    connect(mapStateToProps, mapDispatchToProps),
+    withStyles(styles),
+)(SubjectModal);
---
+++ client/src/pages/FacultyLoading/components/modals/SubjectModal/styles.js
@@ -0,0 +1,9 @@
+import { genericModalStyle, activeItem } from "../../../../../components/styles";
+
+export default theme => ({
+    ...genericModalStyle(theme),
+
+    selectedFaculty: {
+        ...activeItem(theme)
+    },
+})
