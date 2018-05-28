aee4280f3c8e3673ca108de186a3c1404aa76994
Keith <skeithtan@gmail.com>
2018-05-28 14:50:15 +0800

    Added user types to user enums

---
+++ client/src/enums/user.enums.js
@@ -0,0 +1,18 @@
+export const USER_TYPES = {
+    "DEAN": {
+        identifier: "DEAN",
+        name: "Dean",
+    },
+    "ASSOCIATE_DEAN": {
+        identifier: "ASSOCIATE_DEAN",
+        name: "Associate Dean",
+    },
+    "FACULTY": {
+        identifier: "FACULTY",
+        name: "Faculty",
+    },
+    "CLERK": {
+        identifier: "CLERK",
+        name: "Clerk",
+    },
+};
