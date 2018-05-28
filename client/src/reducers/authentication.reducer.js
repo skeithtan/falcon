5d05a0886cb731772d0148e7ca1a737af4177dcf
Keith <skeithtan@gmail.com>
2018-05-28 14:50:50 +0800

    Removed isAuthenticated from redux store

--- client/src/reducers/authentication.reducer.js
+++ client/src/reducers/authentication.reducer.js
@@ -9,7 +9,6 @@ import {
 const hasToken = localStorage.hasOwnProperty("token");
 const hasUser = localStorage.hasOwnProperty("user");
 const initialState = {
-    isAuthenticated: hasToken,
     isLoading: false,
     signInError: null,
     user: hasUser ? JSON.parse(localStorage.user) : null,
@@ -18,28 +17,24 @@ export function authentication(state = initialState, action) {
     switch (action.type) {
         case SIGN_IN_SUCCESS:
             return {
-                isAuthenticated: action.user !== null,
                 isLoading: false,
                 signInError: null,
                 user: action.user,
             };
         case SIGN_IN_IS_LOADING:
             return {
-                isAuthenticated: false,
                 isLoading: true,
                 signInError: null,
                 user: null,
             };
         case SIGN_IN_ERROR:
             return {
-                isAuthenticated: false,
                 isLoading: false,
                 signInError: action.error,
                 user: null,
             };
         case SIGN_OUT_SUCCESS:
             return {
-                isAuthenticated: false,
                 isLoading: false,
                 signInError: null,
                 user: null,
