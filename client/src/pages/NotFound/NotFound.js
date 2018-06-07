36de62c7a6ab9de460324fcfcad183dcf93eb4e8
Keith <skeithtan@gmail.com>
2018-06-07 08:47:55 +0800

    Added go to previous page to NotFound page

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,4 +1,5 @@
 import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
 import React from "react";
 import { HOME_PAGE } from "../index";
 
@@ -10,12 +11,22 @@ export const NotFoundPage = ({classes, history}) => (
             <h1 className={classes.bigMessage}>Oopsie Whoopsie</h1>
             <h1 className={classes.smallMessage}>This page could not be found.</h1>
 
-            <div>
-                <Button variant="raised"
+            <Grid container spacing={8}>
+                <Grid item>
+                    <Button
+                        variant="raised"
+                        onClick={() => history.goBack()}>
+                        Go to previous page
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        variant="raised"
                         onClick={() => history.push(HOME_PAGE.path)}>
-                    Take me home
-                </Button>
-            </div>
+                        Take me home
+                    </Button>
+                </Grid>
+            </Grid>
         </div>
     </div>
 );
