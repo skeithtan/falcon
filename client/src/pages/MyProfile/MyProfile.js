fd7f54ea2cb49c56b91d0083bf30e96e6a7f2c60
Keith <skeithtan@gmail.com>
2018-06-07 09:08:39 +0800

    Added basic MyProfile skeleton

--- client/src/pages/MyProfile/MyProfile.js
+++ client/src/pages/MyProfile/MyProfile.js
@@ -1,4 +1,11 @@
+import Grid from "@material-ui/core/Grid";
 import React, { Component } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { DetailCard } from "../../components/DetailCard";
+import { FullPageLoadingIndicator } from "../../components/FullPageLoadingIndicator";
+import { ErrorState } from "../../components/states/ErrorState";
+import { OVERVIEW_TAB, TABS } from "../FacultyProfiles/components/faculty_detail_tabs";
+import { MY_PROFILE } from "../index";
 import { MyProfileHeader } from "./components/MyProfileHeader";
 
 
@@ -7,11 +14,51 @@ export class MyProfilePage extends Component {
         document.title = "My Profile - Falcon";
     }
 
+    renderTabs = faculty => TABS.map(tab => (
+        <Route
+            key={tab.identifier}
+            path={`/${MY_PROFILE.path}/${tab.path}`}
+            render={() => React.createElement(tab.component, {
+                faculty: faculty,
+            })}
+        />
+    ));
+
+    renderLoading = () => (
+        <Grid container style={{height: "100%"}}>
+            <FullPageLoadingIndicator size={100} />
+        </Grid>
+    );
+
+    renderErrors = errors => (
+        <div className={this.props.classes.cardsContainer}>
+            <DetailCard>
+                <ErrorState onRetryButtonClick={() => this.props.getFacultyDetails(this.props.activeFaculty)}
+                            message="An error occurred while trying to fetch faculty details."
+                            debug={errors[0]} />
+            </DetailCard>
+        </div>
+    );
+
     render() {
         const {classes} = this.props;
+        let faculty = null; // TODO: Redux
+        let errors = null; // TODO: Redux
+
         return (
             <div className={classes.myProfileContainer}>
                 <MyProfileHeader />
+                {!faculty && this.renderLoading()}
+                {errors && this.renderErrors(errors)}
+
+                {faculty &&
+                <Switch>
+                    {this.renderTabs(faculty)}
+                    <Route render={() => (
+                        <Redirect to={`/${MY_PROFILE.path}/${OVERVIEW_TAB.path}`} />
+                    )} />
+                </Switch>
+                }
             </div>
         );
     }
--- client/src/pages/MyProfile/index.js
+++ client/src/pages/MyProfile/index.js
@@ -1,7 +1,8 @@
-import { MyProfilePage as Component } from "./MyProfile";
-import compose from "recompose/compose";
-import { styles } from "./styles";
 import { withStyles } from "@material-ui/core/styles";
+import compose from "recompose/compose";
+import { MyProfilePage as Component } from "./MyProfile";
+import { styles } from "./styles";
+
 
 export const MyProfilePage = compose(
     withStyles(styles),
