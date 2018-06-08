f6f4fadc74d922913436d2f1723dd031d066c85f
Keith <skeithtan@gmail.com>
2018-06-08 09:51:53 +0800

    Added new utility getObjectForUserType

--- client/src/utils/user.util.js
+++ client/src/utils/user.util.js
@@ -1,3 +1,6 @@
+import { ASSOCIATE_DEAN, CLERK, DEAN, FACULTY } from "../enums/user.enums";
+
+
 export function getFullName(user) {
     const name = user.name;
     return `${name.first} ${name.last}`;
@@ -6,4 +9,19 @@ export function getFullName(user) {
 export function getInitials(user) {
     const name = user.name;
     return name.first[0] + name.last[0];
+}
+
+export function getObjectForUserType({user, ifAdministrative, ifFaculty}) {
+    switch (user.authorization) {
+        case DEAN.identifier:
+        case ASSOCIATE_DEAN.identifier:
+        case CLERK.identifier:
+            return ifAdministrative;
+
+        case FACULTY.identifier:
+            return ifFaculty;
+
+        default:
+            throw new Error(`Unknown user authorization type ${user.authorization}`);
+    }
 }
