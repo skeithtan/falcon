76aa8a2b473c5dae585a6673135aee58326bfd46
Keith <skeithtan@gmail.com>
2018-06-11 16:31:36 +0800

    Added search keyword to subject redux state

--- client/src/redux/actions/subject.actions.js
+++ client/src/redux/actions/subject.actions.js
@@ -3,6 +3,14 @@ export const SUBJECT_LIST_IS_FETCHED = "SUBJECT_LIST_IS_FETCHED";
 export const SUBJECT_IS_ADDED = "SUBJECT_IS_ADDED";
 export const SUBJECT_IS_UPDATED = "SUBJECT_IS_UPDATED";
 export const SUBJECT_LIST_FETCH_ERROR = "SUBJECT_LIST_FETCH_ERROR";
+export const SEARCH_KEYWORD_CHANGED = "SUBJECT_LIST_SEARCH_KEYWORD_CHANGED";
+
+export function searchKeywordChanged(searchKeyword) {
+    return {
+        type: SEARCH_KEYWORD_CHANGED,
+        searchKeyword,
+    };
+}
 
 export function subjectListIsLoading() {
     return {
