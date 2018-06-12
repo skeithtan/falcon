d7b77a34b0085713f67bce5bf9926bbe748f205d
Keith <skeithtan@gmail.com>
2018-06-12 09:41:00 +0800

    Removed faculty loading from redux

--- client/src/redux/actions/faculty_loading.actions.js
+++
@@ -1,8 +0,0 @@
-export const FACULTY_LOADING_ACTIVE_TAB_CHANGED = "FACULTY_LOADING_ACTIVE_TAB_CHANGED";
-
-export function facultyLoadingActiveTabChanged(tab) {
-    return {
-        type: FACULTY_LOADING_ACTIVE_TAB_CHANGED,
-        tab,
-    };
-}
--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -2,7 +2,6 @@ import { combineReducers } from "redux";
 import { SIGN_OUT_SUCCESS } from "../actions/authentication.actions";
 import { authentication } from "./authentication.reducer";
 import { faculty } from "./faculty.reducer";
-import { facultyLoading } from "./faculty_loading.reducer";
 import { facultyProfiles } from "./faculty_profiles.reducer";
 import { subject } from "./subject.reducer";
 
@@ -10,7 +9,6 @@ import { subject } from "./subject.reducer";
 const appReducer = combineReducers({
     authentication,
     facultyProfiles,
-    facultyLoading,
     faculty,
     subject,
 });
