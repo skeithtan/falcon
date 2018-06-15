6acb87f6e05bc61a3b8c18029577671320a499cc
Keith <skeithtan@gmail.com>
2018-06-15 12:05:16 +0800

    Enforced permissions in subjects page overview card

--- client/src/pages/Subjects/components/cards/OverviewCard/OverviewCard.js
+++ client/src/pages/Subjects/components/cards/OverviewCard/OverviewCard.js
@@ -1,34 +1,67 @@
 import Grid from "@material-ui/core/Grid";
-import ListItem from "@material-ui/core/ListItem";
+import IconButton from "@material-ui/core/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
+import EditIcon from "@material-ui/icons/Edit";
 import React, { Component } from "react";
 import { DetailCard } from "../../../../../components/DetailCard";
+import { UpdateSubjectModal } from "../../modals/UpdateSubjectModal";
 
 
 export class OverviewCard extends Component {
+    state = {
+        updateSubjectModalIsShowing: false,
+    };
+
+    toggleUpdateSubjectModal = shouldShow => this.setState({
+        updateSubjectModalIsShowing: shouldShow,
+    });
+
     render() {
-        const {subject, classes} = this.props;
+        const {subject, classes, user} = this.props;
+        const {updateSubjectModalIsShowing} = this.state;
+
         return (
             <DetailCard>
-                <ListItem>
-                    <Grid
-                        container
-                        className={classes.overviewTitleContainer}
-                        spacing={8}
-                        direction="column"
-                    >
-                        <Grid item>
-                            <Typography variant="title">
-                                {subject.code}
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography variant="subheading">
-                                {subject.name}
-                            </Typography>
-                        </Grid>
+
+                <div className={classes.buttonArea}>
+                    <div className={classes.buttonsWrapper}>
+                        {user.permissions.MUTATE_FACULTY_PROFILES &&
+                        <Tooltip title="Update these details" placement="left">
+                            <IconButton onClick={() => this.toggleUpdateSubjectModal(true)}>
+                                <EditIcon />
+                            </IconButton>
+                        </Tooltip>
+                        }
+                    </div>
+                </div>
+
+                <Grid
+                    container
+                    className={classes.overviewTitleContainer}
+                    spacing={0}
+                    direction="column"
+                >
+                    <Grid item>
+                        <Typography variant="title">
+                            {subject.code}
+                        </Typography>
                     </Grid>
-                </ListItem>
+                    <Grid item>
+                        <Typography variant="subheading">
+                            {subject.name}
+                        </Typography>
+                    </Grid>
+                </Grid>
+
+                {updateSubjectModalIsShowing &&
+                <UpdateSubjectModal
+                    action="update"
+                    open={updateSubjectModalIsShowing}
+                    onClose={() => this.toggleUpdateSubjectModal(false)}
+                    subject={subject}
+                />
+                }
             </DetailCard>
         );
     }
--- client/src/pages/Subjects/components/cards/OverviewCard/styles.js
+++ client/src/pages/Subjects/components/cards/OverviewCard/styles.js
@@ -1,6 +1,16 @@
 export const styles = theme => ({
     overviewTitleContainer: {
-        padding: theme.spacing.unit * 3,
-        paddingLeft: theme.spacing.unit,
+        padding: theme.spacing.unit * 4,
+        // paddingLeft: theme.spacing.unit,
+    },
+    buttonArea: {
+        display: "flex",
+        position: "absolute",
+        width: "100%",
+        padding: theme.spacing.unit,
+        boxSizing: "border-box",
+    },
+    buttonsWrapper: {
+        marginLeft: "auto",
     },
 });
