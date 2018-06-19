a155c95fd5df4851e59814dd4f48e110fd158342
Keith <skeithtan@gmail.com>
2018-06-19 10:31:11 +0800

    Refactored AddFaculty stages to React Fragments

--- client/src/pages/FacultyProfiles/components/modals/AddFacultyModal/steps/FacultyForm/FacultyForm.js
+++ client/src/pages/FacultyProfiles/components/modals/AddFacultyModal/steps/FacultyForm/FacultyForm.js
@@ -8,7 +8,7 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import TextField from "@material-ui/core/TextField";
-import React from "react";
+import React, { Fragment } from "react";
 import { EMPLOYMENT, SEX } from "../../../../../../../enums/faculty.enums";
 import { validateForm } from "../../../../../../../utils/forms.util";
 
@@ -26,69 +26,75 @@ function getFacultyFormErrors(form) {
 
 export const FacultyForm = ({handleFormChange, form, handleNext, handleBack}) => {
     const {hasErrors, fieldErrors} = getFacultyFormErrors(form);
-    return [
-        <FormControl key={0} error={fieldErrors.birthDate.length > 0} fullWidth>
-            <TextField
-                error={fieldErrors.birthDate.length > 0}
-                label="Date of Birth"
-                type="date"
-                onChange={handleFormChange("birthDate")}
-                value={form.birthDate}
-                InputLabelProps={{
-                    shrink: true,
-                }}
-            />
-            {fieldErrors.birthDate.length > 0 &&
-            <FormHelperText>{fieldErrors.birthDate[0]}</FormHelperText>
-            }
-        </FormControl>,
-        <FormControl error={fieldErrors.idNumber.length > 0} fullWidth>
-            <TextField
-                error={fieldErrors.idNumber.length > 0}
-                label="Faculty ID Number"
-                onChange={handleFormChange("idNumber")}
-                InputProps={{
-                    startAdornment: <InputAdornment position="start">T-</InputAdornment>,
-                }}
-                value={form.idNumber}
-            />
-            {fieldErrors.idNumber.length > 0 &&
-            <FormHelperText>{fieldErrors.idNumber[0]}</FormHelperText>
-            }
-        </FormControl>,
-        <FormControl key={1}>
-            <FormLabel>Sex</FormLabel>
-            <RadioGroup value={form.sex} onChange={handleFormChange("sex")}>
-                {Object.entries(SEX).map(([identifier, {name}]) => (
-                    <FormControlLabel key={identifier} value={identifier} label={name} control={<Radio />} />
-                ))}
-            </RadioGroup>
-        </FormControl>,
-        <FormControl key={2}>
-            <FormLabel>Employment</FormLabel>
-            <RadioGroup value={form.employment} onChange={handleFormChange("employment")}>
-                {Object.entries(EMPLOYMENT).map(([identifier, {name}]) => (
-                    <FormControlLabel key={identifier} value={identifier} label={name} control={<Radio />} />
-                ))}
+    return (
+        <Fragment>
+            <FormControl error={fieldErrors.birthDate.length > 0} fullWidth>
+                <TextField
+                    error={fieldErrors.birthDate.length > 0}
+                    label="Date of Birth"
+                    type="date"
+                    onChange={handleFormChange("birthDate")}
+                    value={form.birthDate}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                {fieldErrors.birthDate.length > 0 &&
+                <FormHelperText>{fieldErrors.birthDate[0]}</FormHelperText>
+                }
+            </FormControl>
 
-            </RadioGroup>
-        </FormControl>,
-        <Grid container key={3} spacing={16}>
-            <Grid item>
-                <Button
-                    onClick={handleBack}>
-                    Back
-                </Button>
+            <FormControl error={fieldErrors.idNumber.length > 0} fullWidth>
+                <TextField
+                    error={fieldErrors.idNumber.length > 0}
+                    label="Faculty ID Number"
+                    onChange={handleFormChange("idNumber")}
+                    InputProps={{
+                        startAdornment: <InputAdornment position="start">T-</InputAdornment>,
+                    }}
+                    value={form.idNumber}
+                />
+                {fieldErrors.idNumber.length > 0 &&
+                <FormHelperText>{fieldErrors.idNumber[0]}</FormHelperText>
+                }
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Sex</FormLabel>
+                <RadioGroup value={form.sex} onChange={handleFormChange("sex")}>
+                    {Object.entries(SEX).map(([identifier, {name}]) => (
+                        <FormControlLabel key={identifier} value={identifier} label={name} control={<Radio />} />
+                    ))}
+                </RadioGroup>
+            </FormControl>
+
+            <FormControl>
+                <FormLabel>Employment</FormLabel>
+                <RadioGroup value={form.employment} onChange={handleFormChange("employment")}>
+                    {Object.entries(EMPLOYMENT).map(([identifier, {name}]) => (
+                        <FormControlLabel key={identifier} value={identifier} label={name} control={<Radio />} />
+                    ))}
+
+                </RadioGroup>
+            </FormControl>
+
+            <Grid container spacing={16}>
+                <Grid item>
+                    <Button
+                        onClick={handleBack}>
+                        Back
+                    </Button>
+                </Grid>
+                <Grid item>
+                    <Button
+                        disabled={hasErrors}
+                        variant="raised"
+                        color="primary"
+                        onClick={handleNext}>
+                        Next
+                    </Button>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Button
-                    disabled={hasErrors}
-                    variant="raised"
-                    color="primary"
-                    onClick={handleNext}>
-                    Next
-                </Button>
-            </Grid>
-        </Grid>,
-    ];
+        </Fragment>
+    );
 };
--- client/src/pages/FacultyProfiles/components/modals/AddFacultyModal/steps/ReviewForm/ReviewForm.js
+++ client/src/pages/FacultyProfiles/components/modals/AddFacultyModal/steps/ReviewForm/ReviewForm.js
@@ -1,6 +1,6 @@
 import List from "@material-ui/core/List";
 import Typography from "@material-ui/core/Typography";
-import React from "react";
+import React, { Fragment } from "react";
 import { FormDisplayListItem } from "../../../../../../../components/FormDisplayListItem";
 import { SEX } from "../../../../../../../enums/faculty.enums";
 
@@ -12,20 +12,22 @@ export const ReviewForm = ({
     const fullName = `${firstName} ${lastName}`;
     const pronoun = sex === SEX.M.identifier ? "his" : "her";
 
-    return [
-        <Typography key={0} variant="body1">
-            Upon signing in for the first time, {fullName} will be
-            prompted to change {pronoun} password for security reasons.
-        </Typography>,
+    return (
+        <Fragment>
+            <Typography variant="body1">
+                Upon signing in for the first time, {fullName} will be
+                prompted to change {pronoun} password for security reasons.
+            </Typography>
 
-        <Typography key={1} variant="body1">
-            <b>Save this temporary password somewhere else</b> as this will be the last time it will be
-            displayed.
-        </Typography>,
+            <Typography variant="body1">
+                <b>Save this temporary password somewhere else</b> as this will be the last time it will be
+                displayed.
+            </Typography>
 
-        <List key={2} className={classes.reviewForm}>
-            <FormDisplayListItem field="Email Address" value={email} />
-            <FormDisplayListItem field="Password" value={password} />
-        </List>,
-    ];
+            <List className={classes.reviewForm}>
+                <FormDisplayListItem field="Email Address" value={email} />
+                <FormDisplayListItem field="Password" value={password} />
+            </List>
+        </Fragment>
+    );
 };
