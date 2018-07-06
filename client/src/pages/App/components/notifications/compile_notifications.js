88419423a4e4eb7b5ab20a3f998d7db722311f64
Keith <skeithtan@gmail.com>
2018-07-06 10:50:24 +0800

    Created compileNotificationsFromState

---
+++ client/src/pages/App/components/notifications/compile_notifications.js
@@ -0,0 +1,92 @@
+import Typography from "@material-ui/core/Typography";
+import React from "react";
+import { UserAvatar } from "../../../../components/UserAvatar/index";
+import { CHANGE_REQUEST_STATUSES } from "../../../../enums/review_profile_change.enums";
+import { makeURL } from "../../../../utils/url.util";
+import { getFullName, getObjectForUserType } from "../../../../utils/user.util";
+import { NotificationItem } from "./NotificationItem/index";
+
+
+const getNotificationsForAdmin = state => {
+    const {
+        changeRequests: {
+            changeRequests,
+            isLoading: changeRequestsIsLoading,
+        },
+        faculty: {
+            faculties,
+            isLoading: facultiesIsLoading,
+        },
+    } = state;
+    const notifications = [];
+
+    // Gather ChangeRequest notifications
+    if (changeRequests && faculties) {
+        Object.entries(changeRequests).forEach(([facultyId, facultyChangeRequests]) => {
+            const faculty = faculties.find(faculty => faculty._id === facultyId);
+            const changeRequestCount = facultyChangeRequests.length;
+
+            notifications.push(onClose => (
+                <NotificationItem
+                    renderAvatar={() => <UserAvatar user={faculty.user} />}
+                    renderText={() => (
+                        <Typography>
+                            <strong>{getFullName(faculty.user)}</strong> submitted <strong>{changeRequestCount} change
+                            requests</strong>
+                        </Typography>
+                    )}
+                    url={makeURL().facultyProfiles().selectFaculty(faculty._id).changeRequests().build()}
+                    onClose={onClose}
+                />
+            ));
+        });
+    }
+
+    return {
+        notifications,
+        isLoading: changeRequestsIsLoading || facultiesIsLoading,
+    };
+};
+
+const getNotificationsForFaculty = state => {
+    const {changeRequests, myProfile} = state;
+    const profile = myProfile.profile;
+    const notifications = [];
+
+    if (changeRequests.changeRequests && profile && changeRequests.changeRequests[profile._id]) {
+        const myChangeRequests = changeRequests.changeRequests[profile._id];
+
+        // Remove all pending
+        myChangeRequests
+            .filter(changeRequest => changeRequest.status !== CHANGE_REQUEST_STATUSES.PENDING.identifier)
+            .forEach(changeRequest => {
+                const verb = CHANGE_REQUEST_STATUSES[changeRequest.status].name;
+
+                notifications.push(onClose => (
+                    <NotificationItem
+                        renderText={() => (
+                            <Typography>
+                                A <strong>{changeRequests.subdocumentType}</strong> change request
+                                titled <strong>{changeRequest.title}</strong> has
+                                been <strong>{verb}</strong>.
+                            </Typography>
+                        )}
+                        url={makeURL().myProfile().changeRequests().build()}
+                        onClose={onClose}
+                    />
+                ));
+            });
+    }
+
+    return {
+        notifications,
+        isLoading: changeRequests.isLoading || myProfile.isLoading,
+    };
+};
+
+export const compileNotificationsFromState = state => getObjectForUserType(state.authentication.user, {
+    DEAN: () => getNotificationsForAdmin(state),
+    ASSOCIATE_DEAN: () => getNotificationsForAdmin(state),
+    CLERK: () => getNotificationsForAdmin(state),
+    FACULTY: () => getNotificationsForFaculty(state),
+})();
