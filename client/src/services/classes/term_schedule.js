0f99536b29ea84f24181e4663212325d09fe1b31
Keith <skeithtan@gmail.com>
2018-07-08 09:39:34 +0800

    Added term schedule to services

---
+++ client/src/services/classes/term_schedule.js
@@ -0,0 +1,158 @@
+import { client } from "../../client";
+import gql from "../../../node_modules/graphql-tag";
+
+const facultyResponseFields = `
+    faculty
+    availability
+    feedback {
+        status
+        rejectionReason
+    }
+`;
+
+const classFields = `
+    _id
+    subject
+    meetingDays
+    meetingHours
+    room
+    enrollmentCap
+    faculty
+    courseCode
+`;
+
+const termScheduleFields = `
+    _id
+    startYear
+    term
+    status
+    facultyPool {
+        ${facultyResponseFields}
+    }
+    classes {
+        ${classFields}
+    }
+`;
+
+export const fetchAllTermSchedules = () =>
+    client.query({
+        query: gql`
+        query {
+            termSchedules {
+                ${termScheduleFields}
+            }
+        }
+    `,
+    });
+
+export const addTermSchedule = (startYear, term) =>
+    client.mutate({
+        mutation: gql`
+            mutation($startYear: Int!, $term: Term!) {
+                addTermSchedule(startYear: $startYear, term: $term) {
+                    ${termScheduleFields}
+                }
+            }
+        `,
+        variables: {
+            startYear,
+            term,
+        },
+    });
+
+export const setFacultyAvailability = (availability, termScheduleId) =>
+    client.mutate({
+        mutation: gql`
+            mutation($availability: JSON!, $termScheduleId: ID!) {
+                setFacultyAvailability(
+                    availaibility: $availability
+                    termScheduleId: $termScheduleId
+                )
+            }
+        `,
+        variables: {
+            availability,
+            termScheduleId,
+        },
+    });
+
+export const setFacultyPool = (termScheduleId, faculties) =>
+    client.mutate({
+        mutation: gql`
+            mutation($termScheduleId: ID!, $faculties: [ID!]!) {
+                termSchedule(_id: $termScheduleId) {
+                    setFacultyPool(faculties: $faculties) {
+                        ${facultyResponseFields}
+                    }
+                }
+            }
+        `,
+        variables: {
+            termScheduleId,
+            faculties,
+        },
+    });
+
+export const addClassToTerm = (termScheduleId, newClass) =>
+    client.mutate({
+        mutation: gql`
+            mutation($termScheduleId: ID!, $newClass: ClassInput!) {
+                termSchedule(_id: $termScheduleId) {
+                    add(newClass: $newClass) {
+                        ${classFields}
+                    }
+                }
+            }
+        `,
+        variables: {
+            termScheduleId,
+            newClass,
+        },
+    });
+
+export const removeClassFromTerm = (termScheduleId, classId) =>
+    client.mutate({
+        mutation: gql`
+            mutation($termScheduleId: ID!, $classId: ID!) {
+                termSchedule(_id: $termScheduleId) {
+                    remove(_id: $classId)
+                }
+            }
+        `,
+        variables: {
+            termScheduleId,
+            classId,
+        },
+    });
+
+export const advanceTermScheduleStatus = termScheduleId =>
+    client.mutate({
+        mutation: gql`
+            mutation($termScheduleId: ID!) {
+                termSchedule(_id: $termScheduleId) {
+                    status {
+                        advance
+                    }
+                }
+            }
+        `,
+        variables: {
+            termScheduleId,
+        },
+    });
+
+export const returnTermScheduleStatus = termScheduleId =>
+    client.mutate({
+        mutation: gql`
+            mutation($termScheduleId: ID!) {
+                termSchedule(_id: $termScheduleId) {
+                    status {
+                        return
+                    }
+                }
+            }
+        `,
+        variables: {
+            termScheduleId,
+        },
+    });
