cbc12f3218b943c4bfde70546e3a90da649af2a6
Keith <skeithtan@gmail.com>
2018-07-09 14:29:10 +0800

    Fixed bug in faculty loading makeURL
    - Also added selectTermSchedule and meeting days

--- client/src/utils/url.util.js
+++ client/src/utils/url.util.js
@@ -1,4 +1,9 @@
-import { FACULTY_PROFILES_PAGE, MY_PROFILE_PAGE, SUBJECTS_PAGE } from "../pages";
+import {
+    FACULTY_PROFILES_PAGE,
+    MY_PROFILE_PAGE,
+    SUBJECTS_PAGE,
+    FACULTY_LOADING_PAGE,
+} from "../pages";
 import {
     CHANGE_REQUESTS_TAB,
     EXTENSION_WORKS_TAB,
@@ -6,7 +11,7 @@ import {
     OVERVIEW_TAB,
     PRESENTATIONS_TAB,
 } from "../pages/FacultyProfiles/components/faculty_detail_tabs";
-
+import { MEETING_DAYS } from "../enums/class.enums";
 
 export const makeURL = () => {
     let currentURL = "";
@@ -18,32 +23,50 @@ export const makeURL = () => {
             build: () => currentURL,
         };
 
-        return subPaths ? {...endpoint, ...subPaths} : endpoint;
+        return subPaths ? { ...endpoint, ...subPaths } : endpoint;
     };
 
     return makeEndpoint(null, {
-        facultyProfiles: () => makeEndpoint(FACULTY_PROFILES_PAGE.path, {
-            selectFaculty: facultyId => makeEndpoint(facultyId, {
+        facultyProfiles: () =>
+            makeEndpoint(FACULTY_PROFILES_PAGE.path, {
+                selectFaculty: facultyId =>
+                    makeEndpoint(facultyId, {
+                        overview: () => makeEndpoint(OVERVIEW_TAB.path),
+                        presentations: () =>
+                            makeEndpoint(PRESENTATIONS_TAB.path),
+                        instructionalMaterials: () =>
+                            makeEndpoint(INSTRUCTIONAL_MATERIALS_TAB.path),
+                        extensionWorks: () =>
+                            makeEndpoint(EXTENSION_WORKS_TAB.path),
+                        changeRequests: () =>
+                            makeEndpoint(CHANGE_REQUESTS_TAB.path),
+                    }),
+            }),
+
+        subjects: () =>
+            makeEndpoint(SUBJECTS_PAGE.path, {
+                selectSubject: subjectId => makeEndpoint(subjectId),
+            }),
+
+        facultyLoading: () =>
+            makeEndpoint(FACULTY_LOADING_PAGE.path, {
+                selectTermSchedule: termScheduleId =>
+                    makeEndpoint(termScheduleId, {
+                        mondayThursday: () =>
+                            makeEndpoint(MEETING_DAYS.M_TH.path),
+                        tuesdayFriday: () =>
+                            makeEndpoint(MEETING_DAYS.T_F.path),
+                    }),
+            }),
+
+        myProfile: () =>
+            makeEndpoint(MY_PROFILE_PAGE.path, {
                 overview: () => makeEndpoint(OVERVIEW_TAB.path),
                 presentations: () => makeEndpoint(PRESENTATIONS_TAB.path),
-                instructionalMaterials: () => makeEndpoint(INSTRUCTIONAL_MATERIALS_TAB.path),
+                instructionalMaterials: () =>
+                    makeEndpoint(INSTRUCTIONAL_MATERIALS_TAB.path),
                 extensionWorks: () => makeEndpoint(EXTENSION_WORKS_TAB.path),
                 changeRequests: () => makeEndpoint(CHANGE_REQUESTS_TAB.path),
             }),
-        }),
-
-        subjects: () => makeEndpoint(SUBJECTS_PAGE.path, {
-            selectSubject: subjectId => makeEndpoint(subjectId),
-        }),
-
-        facultyLoading: () => makeEndpoint(FACULTY_PROFILES_PAGE.path),
-
-        myProfile: () => makeEndpoint(MY_PROFILE_PAGE.path, {
-            overview: () => makeEndpoint(OVERVIEW_TAB.path),
-            presentations: () => makeEndpoint(PRESENTATIONS_TAB.path),
-            instructionalMaterials: () => makeEndpoint(INSTRUCTIONAL_MATERIALS_TAB.path),
-            extensionWorks: () => makeEndpoint(EXTENSION_WORKS_TAB.path),
-            changeRequests: () => makeEndpoint(CHANGE_REQUESTS_TAB.path),
-        }),
     });
 };
