cbb965eb6a47e54566c032753e7ae3efcbad494a
Keith <skeithtan@gmail.com>
2018-07-12 16:38:05 +0800

    Added hover style to class schedule item

--- client/src/pages/FacultyLoading/components/ClassScheduleItem/styles.js
+++ client/src/pages/FacultyLoading/components/ClassScheduleItem/styles.js
@@ -1,18 +1,22 @@
 import { lighten, darken } from "@material-ui/core/styles/colorManipulator";
 import BlueGrey from "@material-ui/core/colors/blueGrey";
 
-
 export const styles = theme => ({
     classScheduleItemContainer: {
         borderLeft: "4px solid",
         padding: theme.spacing.unit * 2,
-        transition: "all 300ms",
+        transition: "all 200ms",
+        cursor: "pointer",
     },
 
     classScheduleWithFaculty: {
         borderColor: theme.palette.primary.main,
         background: lighten(theme.palette.primary.main, 0.92),
         color: theme.palette.primary.dark,
+
+        "&:hover": {
+            background: lighten(theme.palette.primary.main, 0.8),
+        },
 
         "&.selected": {
             background: lighten(theme.palette.primary.main, 0.85),
@@ -23,6 +27,10 @@ export const styles = theme => ({
         borderColor: BlueGrey["300"],
         background: lighten(BlueGrey["400"], 0.95),
         color: darken(BlueGrey["900"], 0.2),
+
+        "&:hover": {
+            background: lighten(BlueGrey["400"], 0.8),
+        },
 
         "&.selected": {
             background: lighten(BlueGrey["400"], 0.85),
