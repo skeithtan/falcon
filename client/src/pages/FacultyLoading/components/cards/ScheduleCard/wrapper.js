e6a24d182d45705609ea5e49046a9a6f5023d8d2
Keith <skeithtan@gmail.com>
2018-07-12 17:52:31 +0800

    Added onSetFaculty to ScheduleCard props

--- client/src/pages/FacultyLoading/components/cards/ScheduleCard/wrapper.js
+++ client/src/pages/FacultyLoading/components/cards/ScheduleCard/wrapper.js
@@ -5,6 +5,9 @@ import { styles } from "./styles";
 import { connect } from "react-redux";
 import { initiatefetchAllFaculties } from "../../../../../utils/faculty.util";
 import { fetchSubjectList } from "../../../../../utils/subject.util";
+import { termScheduleIsUpdated } from "../../../../../redux/actions/faculty_loading.actions";
+import { updateClassSchedule } from "../../../../../services/classes/classes.service";
+import { toastIsShowing } from "../../../../../redux/actions/toast.actions";
 
 const mapStateToProps = state => ({
     faculties: state.faculty,
@@ -17,6 +20,48 @@ const mapDispatchToProps = dispatch => ({
     },
     fetchAllSubjects() {
         fetchSubjectList(dispatch);
+    },
+    onSetFaculty(faculty, oldClassSchedule, termSchedule) {
+        const newClassSchedule = {
+            ...oldClassSchedule,
+            faculty: faculty._id,
+        };
+
+        const oldTermSchedule = { ...termSchedule };
+
+        const newTermSchedule = {
+            ...oldTermSchedule,
+            classes: oldTermSchedule.classes.map(classSchedule => {
+                if (newClassSchedule._id === classSchedule._id) {
+                    return newClassSchedule;
+                }
+
+                return classSchedule;
+            }),
+        };
+
+        const revertToOldSchedule = errors => {
+            console.log(errors);
+            dispatch(termScheduleIsUpdated(oldTermSchedule));
+            dispatch(
+                toastIsShowing(
+                    "An error occurred while updating class schedule"
+                )
+            );
+        };
+
+        dispatch(termScheduleIsUpdated(newTermSchedule));
+        updateClassSchedule(
+            newTermSchedule._id,
+            newClassSchedule._id,
+            newClassSchedule
+        )
+            .then(result => {
+                if (result.errors) {
+                    revertToOldSchedule(result.errors);
+                }
+            })
+            .catch(error => revertToOldSchedule(error));
     },
 });
 
