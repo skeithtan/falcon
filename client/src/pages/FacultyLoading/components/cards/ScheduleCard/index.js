eccbf3b68d5ef3cfd2984c300dc16cc2e4e83442
Keith <skeithtan@gmail.com>
2018-07-12 15:37:57 +0800

    Added faculties and subjects fetching to ScheduleCard

--- client/src/pages/FacultyLoading/components/cards/ScheduleCard/index.js
+++ client/src/pages/FacultyLoading/components/cards/ScheduleCard/index.js
@@ -1,24 +1,231 @@
 import Card from "@material-ui/core/Card";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import React, { PureComponent } from "react";
-import { meetingDaysFromPath } from "../../../../../utils/faculty_loading.util";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Grid from "@material-ui/core/Grid";
+import Tab from "@material-ui/core/Tab";
+import Tabs from "@material-ui/core/Tabs";
+import React, { Component } from "react";
 import { ScheduleCalendar } from "../../ScheduleCalendar";
 import Divider from "@material-ui/core/Divider";
+import { makeURL } from "../../../../../utils/url.util";
+import { wrap } from "./wrapper";
+import { FACULTY_LOADING_PAGE } from "../../../..";
+import { MEETING_DAYS } from "../../../../../enums/class.enums";
+import { FullPageLoadingIndicator } from "../../../../../components/FullPageLoadingIndicator";
+import { ErrorState } from "../../../../../components/states/ErrorState";
 
-export class ScheduleCard extends PureComponent {
-    render() {
-        const { activeTermSchedule, meetingDays: meetingDaysPath } = this.props;
-        const meetingDays = meetingDaysFromPath(meetingDaysPath);
+class BaseScheduleCard extends Component {
+    state = {
+        showOnlyUnassigned: false,
+    };
+
+    handleSetOnlyUnassignedCheckbox = ({ target: { checked } }) =>
+        this.setState({
+            showOnlyUnassigned: checked,
+        });
+
+    componentDidMount() {
+        this.handlePath();
+        this.fetchData();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        this.handlePath();
+        this.fetchData();
+    }
+
+    meetingDaysPathIsValid = meetingDays =>
+        Object.values(MEETING_DAYS)
+            .map(({ path }) => path)
+            .includes(meetingDays);
+
+    fetchData = () => {
+        const {
+            faculties,
+            subjects,
+            fetchAllFaculties,
+            fetchAllSubjects,
+        } = this.props;
+
+        if (!faculties.faculties && !faculties.isLoading && !faculties.errors) {
+            fetchAllFaculties();
+        }
+
+        if (!subjects.subjects && !subjects.isLoading && !subjects.errors) {
+            fetchAllSubjects();
+        }
+    };
+
+    handlePath = () => {
+        const { meetingDays, activeTermSchedule, history } = this.props;
+        if (!meetingDays) {
+            history.replace(
+                makeURL()
+                    .facultyLoading()
+                    .selectTermSchedule(activeTermSchedule._id)
+                    .mondayThursday()
+                    .build()
+            );
+        }
+
+        if (!this.meetingDaysPathIsValid(meetingDays)) {
+            history.replace(
+                makeURL()
+                    .facultyLoading()
+                    .selectTermSchedule(activeTermSchedule._id)
+                    .mondayThursday()
+                    .build()
+            );
+        }
+    };
+
+    onTabClick = path => {
+        const { meetingDays, history, activeTermSchedule } = this.props;
+
+        if (path === meetingDays) {
+            return;
+        }
+
+        history.push(
+            `/${FACULTY_LOADING_PAGE.path}/${activeTermSchedule._id}/${path}`
+        );
+    };
+
+    renderToolbar = () => {
+        const { meetingDays } = this.props;
+        return (
+            <Grid container justify="space-between">
+                <Grid item xs>
+                    <Tabs
+                        value={meetingDays}
+                        indicatorColor="primary"
+                        textColor="primary"
+                    >
+                        {Object.values(MEETING_DAYS).map(({ name, path }) => (
+                            <Tab
+                                key={path}
+                                label={name}
+                                value={path}
+                                onClick={() => this.onTabClick(path)}
+                            />
+                        ))}
+                    </Tabs>
+                </Grid>
+                <Grid item>
+                    <FormControlLabel
+                        label="Show only unassigned classes"
+                        control={
+                            <Checkbox
+                                checked={this.state.showOnlyUnassigned}
+                                onChange={this.handleSetOnlyUnassignedCheckbox}
+                            />
+                        }
+                    />
+                </Grid>
+            </Grid>
+        );
+    };
+
+    getClassSchedules = () => {
+        const { meetingDays: meetingDaysPath, activeTermSchedule } = this.props;
+        const { showOnlyUnassigned } = this.state;
+        let meetingDayKey = MEETING_DAYS.M_TH.identifier;
+
+        if (this.meetingDaysPathIsValid(meetingDaysPath)) {
+            const meetingDay = Object.values(MEETING_DAYS).find(
+                ({ path }) => path === meetingDaysPath
+            );
+
+            meetingDayKey = meetingDay.identifier;
+        }
+
+        let classSchedules = activeTermSchedule.classes.filter(
+            classSchedule => classSchedule.meetingDays === meetingDayKey
+        );
+
+        if (showOnlyUnassigned) {
+            classSchedules = classSchedules.filter(
+                classSchedule => classSchedule.faculty === null
+            );
+        }
+
+        return classSchedules;
+    };
+
+    renderLoading = () => (
+        <Grid container style={{ height: "100%" }}>
+            <FullPageLoadingIndicator size={100} />
+        </Grid>
+    );
+
+    renderErrors = (errors, onErrorRetryButtonClick) => (
+        <ErrorState
+            onRetryButtonClick={onErrorRetryButtonClick}
+            message="An error occurred while trying to fetch data."
+            debug={errors[0]}
+        />
+    );
+
+    renderCardContent = () => {
+        const {
+            classes,
+            faculties,
+            subjects,
+            fetchAllFaculties,
+            fetchAllSubjects,
+        } = this.props;
+
+        const isLoading = faculties.isLoading || subjects.isLoading;
+        const errors = faculties.errors || subjects.errors;
+        const onErrorRetryButtonClick = faculties.errors
+            ? fetchAllFaculties
+            : fetchAllSubjects;
+
+        if (isLoading) {
+            return this.renderLoading();
+        }
+
+        if (errors) {
+            return this.renderErrors(errors, onErrorRetryButtonClick);
+        }
+
+        const classSchedules = this.getClassSchedules();
+
+        if (subjects.subjects === null || faculties.faculties === null) {
+            return null;
+        }
 
         return (
-            <Card>
-                <Toolbar>
-                    <Typography variant="title">{meetingDays.name}</Typography>
-                </Toolbar>
-                <Divider />
-                <ScheduleCalendar />
+            <Grid
+                className={classes.scheduleCardContainer}
+                container
+                direction="column"
+                wrap="nowrap"
+            >
+                <Grid item>{this.renderToolbar()}</Grid>
+                <Grid item>
+                    <Divider light />
+                </Grid>
+                <Grid item xs>
+                    <ScheduleCalendar
+                        classSchedules={classSchedules}
+                        faculties={faculties.faculties}
+                        subjects={subjects.subjects}
+                    />
+                </Grid>
+            </Grid>
+        );
+    };
+
+    render() {
+        const { classes } = this.props;
+
+        return (
+            <Card className={classes.scheduleCardContainer}>
+                {this.renderCardContent()}
             </Card>
         );
     }
 }
+
+export const ScheduleCard = wrap(BaseScheduleCard);
---
+++ client/src/pages/FacultyLoading/components/cards/ScheduleCard/wrapper.js
@@ -0,0 +1,30 @@
+import compose from "recompose/compose";
+import { withRouter } from "react-router-dom";
+import { withStyles } from "@material-ui/core/styles";
+import { styles } from "./styles";
+import { connect } from "react-redux";
+import { initiatefetchAllFaculties } from "../../../../../utils/faculty.util";
+import { fetchSubjectList } from "../../../../../utils/subject.util";
+
+const mapStateToProps = state => ({
+    faculties: state.faculty,
+    subjects: state.subject,
+});
+
+const mapDispatchToProps = dispatch => ({
+    fetchAllFaculties() {
+        initiatefetchAllFaculties(dispatch);
+    },
+    fetchAllSubjects() {
+        fetchSubjectList(dispatch);
+    },
+});
+
+export const wrap = compose(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    ),
+    withStyles(styles),
+    withRouter
+);
