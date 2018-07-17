6ea68528948e770e406fd47a6a76782c2145884a
Keith <skeithtan@gmail.com>
2018-07-17 16:39:54 +0800

    Fixed MyProfileHeader tabs

--- client/src/pages/MyProfile/components/MyProfileHeader/styles.js
+++ client/src/pages/MyProfile/components/MyProfileHeader/styles.js
@@ -2,13 +2,19 @@ import { appBarExtension } from "../../../../components/styles";
 
 export const styles = theme => ({
     header: {
-        ...appBarExtension(theme)
+        ...appBarExtension(theme),
+        display: "flex",
+        paddingLeft: theme.spacing.unit * 2,
+        paddingRight: theme.spacing.unit * 2,
     },
     tabs: {
         color: theme.palette.common.white,
         "&:focus": {
             color: "#fff",
         },
+    },
+    tabsFlexContainer: {
+        height: "100%",
     },
     tabsIndicator: {
         background: theme.palette.common.white,
