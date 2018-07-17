7410fef09e08a5c09dccfe869322f65751fc5bcb
Keith <skeithtan@gmail.com>
2018-07-17 12:03:36 +0800

    Added mySchedule query
    - Replaced schedule field with class array

--- server/graphql/resolvers/faculty_schedule.resolver.js
+++ server/graphql/resolvers/faculty_schedule.resolver.js
@@ -1,14 +1,64 @@
 import { limitAccess } from "../../utils/user_decorator";
-import { FACULTY } from "../../../client/src/enums/user.enums";
+import { FACULTY } from "../../models/user.model";
+import { getUserFromContext } from "../../utils/user_from_context";
+import { Faculty } from "../../models/faculty.model";
+import { TermSchedule } from "../../models/class.model";
+
+const termScheduleToFacultyFormat = (termSchedule, faculty) => {
+    const facultyResponse = termSchedule.facultyPool.find(
+        facultyResponse => facultyResponse.faculty === faculty._id
+    );
+    const classes = termSchedule.classes
+        // Remove all classes without faculties
+        .filter(assignedFaculty => assignedFaculty !== null)
+        // Get only classes whose faculty is the current user
+        .filter(assignedFaculty => assignedFaculty._id === faculty._id);
+
+    return {
+        status: termSchedule.status,
+        startYear: termSchedule.startYear,
+        term: termSchedule.term,
+        involved: facultyResponse !== null,
+        classes,
+        availability: facultyResponse ? facultyResponse.availability : null,
+        feedback: facultyResponse ? facultyResponse.feedback : null,
+    };
+};
+
+const getCurrentSchedule = async faculty => {
+    const currentTermSchedule =
+        // Regex specifies that the status string must be exactly "ARCHIVED"
+        // More information: https://simple-regex.com/build/5b4c4ffbba250
+        await TermSchedule.findOne({
+            status: { $not: /(?:ARCHIVED)$/ },
+        }).exec();
+
+    return termScheduleToFacultyFormat(currentTermSchedule, faculty);
+};
+
+const getArchivedSchedule = async faculty => {
+    const archivedTermSchedules = await TermSchedule.find({
+        status: "ARCHIVED",
+    }).exec();
+    return archivedTermSchedules.map(termSchedule =>
+        termScheduleToFacultyFormat(termSchedule, faculty)
+    );
+};
+
+const mySchedule = async (object, args, context) => {
+    const user = await getUserFromContext(context);
+    const faculty = await Faculty.findOne({ user: user._id }).exec();
+
+    return {
+        current: getCurrentSchedule(faculty),
+        archived: getArchivedSchedule(faculty),
+    };
+};
 
 export const queryResolvers = {
-    current: limitAccess(null, {
+    mySchedule: limitAccess(mySchedule, {
         allowed: FACULTY,
-        action: "View current faculty current term schedule",
-    }),
-    archived: limitAccess(null, {
-        allowed: FACULTY,
-        action: "View current faculty archived terms schedules",
+        action: "View my schedule",
     }),
 };
 
--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -1,13 +1,25 @@
 import GraphQLJSON from "graphql-type-json";
-import { mutationResolvers as classMutations, queryResolvers as classQueries } from "./class.resolver";
-import { mutationResolvers as facultyMutations, queryResolvers as facultyQueries } from "./faculty.resolver";
+import {
+    mutationResolvers as classMutations,
+    queryResolvers as classQueries,
+} from "./class.resolver";
+import {
+    mutationResolvers as facultyMutations,
+    queryResolvers as facultyQueries,
+} from "./faculty.resolver";
 import { mutationResolvers as requestProfileChangesMutations } from "./request_profile_changes.resolver";
 import {
     mutationResolvers as reviewProfileChangesMutations,
     queryResolvers as profileChangesQueries,
 } from "./review_profile_changes.resolver";
-import { mutationResolvers as userMutations, queryResolvers as userQueries } from "./user.resolver";
-
+import {
+    mutationResolvers as facultyScheduleMutations,
+    queryResolvers as facultySchedulesQueries,
+} from "./faculty_schedule.resolver";
+import {
+    mutationResolvers as userMutations,
+    queryResolvers as userQueries,
+} from "./user.resolver";
 
 const helloWorldResolver = () => "World";
 
@@ -17,6 +29,7 @@ const Query = {
     ...classQueries,
     ...facultyQueries,
     ...profileChangesQueries,
+    ...facultySchedulesQueries,
 };
 
 const Mutation = {
@@ -26,6 +39,7 @@ const Mutation = {
     ...facultyMutations,
     ...requestProfileChangesMutations,
     ...reviewProfileChangesMutations,
+    ...facultyScheduleMutations,
 };
 
 const typeDefs = {
