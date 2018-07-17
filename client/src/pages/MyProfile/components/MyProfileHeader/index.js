dec2fe44560d2d338474640301b9a36c1ab58183
Keith <skeithtan@gmail.com>
2018-07-17 16:09:37 +0800

    Fixed MyProfileHeader tabs

--- client/src/pages/MyProfile/components/MyProfileHeader/index.js
+++ client/src/pages/MyProfile/components/MyProfileHeader/index.js
@@ -5,22 +5,31 @@ import { TABS } from "../../../FacultyProfiles/components/faculty_detail_tabs";
 import { MY_PROFILE_PAGE } from "../../..";
 import { wrap } from "./wrapper";
 
-
 class BaseMyProfileHeader extends PureComponent {
-    renderMyProfileTabs = ({history}) => TABS.map(({identifier, name, path}) => (
-        <Tab key={identifier} label={name} onClick={() => history.push(`/${MY_PROFILE_PAGE.path}/${path}`)} />
-    ));
+    renderMyProfileTabs = ({ history }) =>
+        TABS.map(({ identifier, name, path }) => (
+            <Tab
+                key={identifier}
+                label={name}
+                onClick={() => history.push(`/${MY_PROFILE_PAGE.path}/${path}`)}
+            />
+        ));
 
     render() {
-        const {classes, match, history} = this.props;
-        const activeTabIndex = TABS.findIndex(tab => tab.path === match.params.activeTab) || 0;
+        const { classes, match, history } = this.props;
+        const activeTabIndex =
+            TABS.findIndex(tab => tab.path === match.params.activeTab) || 0;
+
+        const tabsClasses = {
+            root: classes.tabs,
+            indicator: classes.tabsIndicator,
+            flexContainer: classes.tabsFlexContainer,
+        };
+
         return (
             <div className={classes.header}>
-                <Tabs
-                    value={activeTabIndex}
-                    classes={{root: classes.tabs, indicator: classes.tabsIndicator}}
-                >
-                    {this.renderMyProfileTabs({history})}
+                <Tabs value={activeTabIndex} classes={tabsClasses}>
+                    {this.renderMyProfileTabs({ history })}
                 </Tabs>
             </div>
         );
